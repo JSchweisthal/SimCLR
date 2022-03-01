4832f8db38441704151aedb21b3a824563828d8d
Jonas <jonas.schweisthal@campus.lmu.de>
2022-03-01 22:38:18 +0100

    mc

--- linear_evaluation_binary.py
+++ linear_evaluation_binary.py
@@ -260,7 +260,7 @@ if __name__ == "__main__":
             train_datasubset_pu = torch.utils.data.Subset(train_dataset, idxs) 
         
         if args.data_classif == "PU":
-            test_dataset.targets = torch.where(torch.isin(test_dataset.targets.targets, torch.tensor([0, 1, 8, 9])), 1, 0)  
+            test_dataset.targets = torch.where(torch.isin(test_dataset.targets, torch.tensor([0, 1, 8, 9])), 1, 0)  
     else:
         raise NotImplementedError
 
