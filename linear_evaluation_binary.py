343408e38fba9246c0d382f8e2d83de8e2a1b42f
Jonas <jonas.schweisthal@campus.lmu.de>
2022-04-09 20:14:05 +0200

    mc

--- linear_evaluation_binary.py
+++ linear_evaluation_binary.py
@@ -302,7 +302,10 @@ if __name__ == "__main__":
             # idxs_test = [i for i in range(len(test_dataset.targets)) if test_dataset.targets[i] in [args.class_pos, args.class_neg]]
 
             idxs_test = [i for i in range(len(test_dataset.targets)) if test_dataset.targets[i] == args.class_pos]
+            import random
+            random.shuffle(idxs_test)
             idxs_test = idxs_test[:750]
+            idxs_test.sort()
             idxs_test_neg = [i for i in range(len(test_dataset.targets)) if test_dataset.targets[i] == args.class_neg]
             idxs_test.extend(idxs_test_neg)
 
